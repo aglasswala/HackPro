44bff2e5388097a5e76fb0c2bce0e2394d153df6
Aadil Glasswala <aglasswala@gmail.com>
2018-11-11 10:40:54 -0500

    fixed more bugs

--- script.js
+++ script.js
@@ -1,36 +1,9 @@
-
-function createCalender() {
-
-    const a = $('<div class="row justify-content-center"><div class="card-group"></div></div>').appendTo("#cardDisplay");
-    $('<a href="" data-toggle="modal" data-target="#1" class="custom-card"><div class="card" style="height: 50vh; width: 15vw;"><h5 class="card-header">3</h5><div class="card-body"><ul class="list-group" id="one1"></ul></a></div>').appendTo(a);
-    $('<a href="" data-toggle="modal" data-target="#2" class="custom-card"><div class="card" style="height: 50vh; width: 15vw;"><h5 class="card-header">4</h5><div class="card-body"><ul class="list-group" id="one2"></ul></a></div>').appendTo(a);
-    $('<a href="" data-toggle="modal" data-target="#3" class="custom-card"><div class="card" style="height: 50vh; width: 15vw;"><h5 class="card-header">5</h5><div class="card-body"><ul class="list-group" id="one3"></ul></a></div>').appendTo(a);
-    $('<a href="" data-toggle="modal" data-target="#4" class="custom-card"><div class="card" style="height: 50vh; width: 15vw;"><h5 class="card-header">6</h5><div class="card-body"><ul class="list-group" id="one4"></ul></a></div>').appendTo(a);
-    $('<a href="" data-toggle="modal" data-target="#5" class="custom-card"><div class="card" style="height: 50vh; width: 15vw;"><h5 class="card-header">7</h5><div class="card-body"><ul class="list-group" id="one5"></ul></a></div>').appendTo(a);
-
-
-}
-
 $(function () {
   $("#datepicker").datepicker({ 
         autoclose: true, 
         todayHighlight: true
   }).datepicker('update', new Date());
 });
-
-
-// <div class="input-group"><input type="text" class="form-control" placeholder="subject" id="course1"><select class="custom-select" id="time1"> <option selected>1:00</option><option value="1:30">1:30</option><option value="2:00">2:00</option><option value="2:30">2:30</option></select></div><button class="input-group-text btn btn-outline-info" onclick="run(one1, course1, time1)">Add</button>
-// <div class="input-group"><input type="text" class="form-control" placeholder="subject" id="course2"><select class="custom-select" id="time2"> <option selected>1:00</option><option value="1:30">1:30</option><option value="2:00">2:00</option><option value="2:30">2:30</option></select></div><button class="input-group-text btn btn-outline-info" onclick="run(one2, course2, time2)">Add</button>
-// <div class="input-group"><input type="text" class="form-control" placeholder="subject" id="course3"><select class="custom-select" id="time3"> <option selected>1:00</option><option value="1:30">1:30</option><option value="2:00">2:00</option><option value="2:30">2:30</option></select></div><button class="input-group-text btn btn-outline-info" onclick="run(one3, course3, time3)">Add</button>
-// <div class="input-group"><input type="text" class="form-control" placeholder="subject" id="course4"><select class="custom-select" id="time4"> <option selected>1:00</option><option value="1:30">1:30</option><option value="2:00">2:00</option><option value="2:30">2:30</option></select></div><button class="input-group-text btn btn-outline-info" onclick="run(one4, course4, time4)">Add</button>
-// <div class="input-group"><input type="text" class="form-control" placeholder="subject" id="course5"><select class="custom-select" id="time5"> <option selected>1:00</option><option value="1:30">1:30</option><option value="2:00">2:00</option><option value="2:30">2:30</option></select></div><button class="input-group-text btn btn-outline-info" onclick="run(one5, course5, time5)">Add</button>
-
-
-// function run(ulId, courseNum, timeNum) {
-//   const course = courseNum.value;
-//   const time = timeNum.value;
-//   $('<li class="list-group-item"><div class="row justify-content-between"><div class="col-sm">' + course +'</div><div class="col-sm">'+ time + '</div></div></li>').appendTo(ulId)
-// }
 
 function run(courseNum, whereOne, timeNum, nameNum, dateNum) {
   const where = whereOne.value;
@@ -119,11 +92,10 @@ function signIn() {
       break;
     } else {
       document.getElementById("demo").innerHTML = "Wrong password or Email"
-
     }
   }
-
 }
+
 function register(){
     var firstName = document.getElementById("inputFirstname").value;
     var lastName = document.getElementById("inputLastname").value;
@@ -131,10 +103,6 @@ function register(){
     var password = document.getElementById("inputPassword").value;
     document.location.href = "index.html";
 }
-
-
-
-
 
 $(function () {
    $('#modalfirst').modal(toggle)
