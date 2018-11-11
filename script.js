59107a989f41663a0be8722792fe55d055d24086
Aadil Glasswala <aglasswala@gmail.com>
2018-11-11 09:50:49 -0500

    changed up design and added new features

--- script.js
+++ script.js
@@ -11,6 +11,13 @@ function createCalender() {
 
 }
 
+$(function () {
+  $("#datepicker").datepicker({ 
+        autoclose: true, 
+        todayHighlight: true
+  }).datepicker('update', new Date());
+});
+
 
 // <div class="input-group"><input type="text" class="form-control" placeholder="subject" id="course1"><select class="custom-select" id="time1"> <option selected>1:00</option><option value="1:30">1:30</option><option value="2:00">2:00</option><option value="2:30">2:30</option></select></div><button class="input-group-text btn btn-outline-info" onclick="run(one1, course1, time1)">Add</button>
 // <div class="input-group"><input type="text" class="form-control" placeholder="subject" id="course2"><select class="custom-select" id="time2"> <option selected>1:00</option><option value="1:30">1:30</option><option value="2:00">2:00</option><option value="2:30">2:30</option></select></div><button class="input-group-text btn btn-outline-info" onclick="run(one2, course2, time2)">Add</button>
@@ -19,11 +26,73 @@ function createCalender() {
 // <div class="input-group"><input type="text" class="form-control" placeholder="subject" id="course5"><select class="custom-select" id="time5"> <option selected>1:00</option><option value="1:30">1:30</option><option value="2:00">2:00</option><option value="2:30">2:30</option></select></div><button class="input-group-text btn btn-outline-info" onclick="run(one5, course5, time5)">Add</button>
 
 
-function run(ulId, courseNum, timeNum) {
+// function run(ulId, courseNum, timeNum) {
+//   const course = courseNum.value;
+//   const time = timeNum.value;
+//   $('<li class="list-group-item"><div class="row justify-content-between"><div class="col-sm">' + course +'</div><div class="col-sm">'+ time + '</div></div></li>').appendTo(ulId)
+// }
+
+function run(courseNum, timeNum, nameNum, dateNum) {
   const course = courseNum.value;
   const time = timeNum.value;
-  $('<li class="list-group-item"><div class="row justify-content-between"><div class="col-sm">' + course +'</div><div class="col-sm">'+ time + '</div></div></li>').appendTo(ulId)
+  let name  = nameNum.value;
+
+  const b = $('<tr></tr>').appendTo('#tableMain');
+
+  const a = getDate(dateNum.value, b);
+
+  name = name + " - " + course;
+
+  $('<td class="agenda-time">' + time + '</td>').appendTo(b);
+  $('<td class="agenda-events"><div class="agenda-event"><i class="glyphicon glyphicon-repeat text-muted" title="Repeating event"></i>' + name + '</div></td>').appendTo(b);
+
 }
+
+function getDate(x, b) {
+  var arr = x.split("-");
+  var month = arr[0];
+  var day = arr[1];
+  var year = arr[2];
+
+
+  if(month === 1) month = "Jan";
+  else if(month === "2 ") month = "Feb";
+  else if(month === "3 ") month = "Mar";
+  else if(month === "4 ") month = "Apr";
+  else if(month === "5 ") month = "May";
+  else if(month === "6 ") month = "Jun";
+  else if(month === "7 ") month = "Jul";
+  else if(month === "8 ") month = "Aug";
+  else if(month === "9 ") month = "Sep";
+  else if(month === "10") month = "Oct";
+  else if(month === "11") month = "Nov";
+  else if(month === "12") month = "Dec"; 
+  
+  month = month + " " + year;
+
+  var thur = 1;
+  var fri = 2;
+  var sat = 3;
+  var sun = 4;
+  var mon = 5;
+  var tue = 6
+  var wed = 7;
+
+  if(day % 7 === thur) day = "Thursday";
+  else if(day % 7 === fri) day = "Friday";
+  else if(day % 7 === sat) day = "Saturday";
+  else if(day % 7 === sun) day = "Sunday";
+  else if(day % 7 === mon) day = "Monday";
+  else if(day % 7 === tue) day = "Tuesday";
+  else if(day % 7 === wed) day = "Wednesday";
+
+  $('<td class="agenda-date" class="active" rowspan="1"><div class="dayofmonth">' + arr[1] + '</div><div class="dayofweek">' + day + '</div><div class="shortdate text-muted">' + month + '</div></td>').appendTo(b);
+
+
+}
+
+
+
 
 
 var db =
